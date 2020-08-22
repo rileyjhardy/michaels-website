0251a60967f96fbb832625bccfb5d7752ba33e7c
Riley Hardy <rileyhardy@pop-os.localdomain>
2020-08-22 10:07:56 -0500

    "third commit"

--- src/contact.js
+++ src/contact.js
@@ -9,9 +9,6 @@ const Contact = (props) => {
             <div className = "close-menu-black" onClick={props.toggle}>
                 <CloseIcon />
                 </div>
-                <div className = "contact-container">
-                    <h1 className = "secondary-michael" data-aos = "fade-right">MICHAEL KELLEY</h1>
-                    <div className = "vert-divider" data-aos = "zoom-in"></div>
                     <div className = "contact">
                         <div className = "action-links">
                             <a href="mailto: michaelkelleyfilm@gmail.com" data-aos = "flip-up">
@@ -32,9 +29,9 @@ const Contact = (props) => {
                             <a  data-aos = "flip-left" data-aos-delay = "100" data-aos-anchor-placement="bottom-bottom"href="https://www.instagram.com/kelley_michael" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram about-icons"></i></a>
                             <a  data-aos = "flip-left" data-aos-delay = "200" data-aos-anchor-placement="bottom-bottom"href="https://www.linkedin.com/in/michael-kelley-b64b70b7/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin about-icons"></i></a>
                             <a  data-aos = "flip-left" data-aos-delay = "300" data-aos-anchor-placement="bottom-bottom"href="https://vimeo.com/michaelkelley" target="_blank" rel="noopener noreferrer"><i className="fab fa-vimeo-v about-icons"></i></a>
-                            <a  data-aos = "flip-left" data-aos-delay = "400" data-aos-anchor-placement="bottom-bottom"href="https://www.imdb.com/name/nm8066625/?ref_=fn_nm_nm_14" target="_blank" rel="noopener noreferrer"><i class="fab fa-imdb about-icons"></i></a>
+                            <a  data-aos = "flip-left" data-aos-delay = "400" data-aos-anchor-placement="bottom-bottom"href="https://www.imdb.com/name/nm6512040/?ref_=fn_nm_nm_155" target="_blank" rel="noopener noreferrer"><i class="fab fa-imdb about-icons"></i></a>
                         </div>
-                </div>
+                
             </div>
         </div>
      </Fade>
